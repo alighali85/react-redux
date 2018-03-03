61d63c61d0f5c3af139e7c3d9fd9e37978b6ca43
Ali <ali@alighali.com>
2018-03-03 17:45:57 +0100

    add reucers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { voteAngular, voteReact, voteVuejs } from './actions';
 
 class App extends Component {
+
+  constructor( props ) {
+    super( props );
+    this.store = this.props.store;
+  }
+
+  handleVoteAngular = ()=>  {
+    this.store.dispatch( voteAngular() );
+  }
+
+  handleVoteReact = ()=>  {
+    this.store.dispatch( voteReact() );
+  }
+
+  handleVoteVuejs = ()=>  {
+    this.store.dispatch( voteVuejs() );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+    <div>
+        <div className='jumbotron' style={{ textAlign : 'center'}}>
+          <img src='ctsw_logo.png' height='96'></img>
+          <h2> what is your favorite front-end framework in 2018 ?</h2>
+          <h4> click on the logo below to vote! </h4>
+          <br/>
+          
+          <div className='row text-center'>
+          <div className='col-md-3'></div>
+            <div className ='col-md-offset-3 col-md-2'>
+              <img src='angular_logo.png' height='96' onClick={ this.handleVoteAngular }>
+              </img>
+            </div>
+
+            <div className ='col-xs-2 col-md-2'>
+              <img src='react_logo.png' height='96' onClick={ this.handleVoteReact }>
+              </img>
+            </div>
+
+            <div className ='col-xs-2 col-md-2'>
+              <img src='vue_logo.png' height='96' onClick={ this.handleVoteVuejs }>
+              </img>
+            </div>
+        </div>
+
+
       </div>
+    </div>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+
+// create Initial staet to  statrt the application with 
+
+const initialState = {
+    angular: 0,
+    react: 0,
+    vuejs: 0
+}
+
+// create the reducers 
+export default( state = initialState, action )=> {
+    switch( action.type ) {
+
+        case 'VOTE_ANGULAR':
+        console.log('Vote to angular ');
+        return Object.assign( {}, state, {
+            angular: state.angular + 1
+        })
+
+        case 'VOTE_REACT':
+        console.log('Vote to react ');
+        return Object.assign( {}, state, {
+            react: state.react + 1 
+        })
+
+        case 'VOTE_VUEJS':
+        console.log('Vote to vuejs ')
+        return Object.assign( {}, state, {
+            vuejs: state.vuejs + 1 
+        })
+
+        default:
+        return state
+    }
+}
